2127ede7232764d2bf5178783c7a87b48e69ed35
James <jamesb88@hotmail.co.uk>
2023-06-01 19:52:40 +0100

    webpack and bootstrap installed

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path")
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+        test: /\.scss$/,
+        use: [
+          "style-loader", //3. Inject styles into DOM
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader", //1. Turns sass into css
+        ],
+      },
+    ],
+  },
+}
